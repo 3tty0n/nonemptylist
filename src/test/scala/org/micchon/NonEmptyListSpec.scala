6d47629fb2137d6d6c4eee1fb9994018638bec37
Yusuke Izawa <yuizalp@gmail.com>
2017-06-11 20:03:01 +0900

    change the trait for the test

--- src/test/scala/org/micchon/NonEmptyListSpec.scala
+++ src/test/scala/org/micchon/NonEmptyListSpec.scala
@@ -1,63 +1,61 @@
 package org.micchon
 
-import org.scalatest.FlatSpec
-import org.scalatest.FunSpec
+import org.scalatest.FunSuite
+
 import scala.collection.mutable.ListBuffer
 
-class NonEmptyListSpec extends FlatSpec with FunSpec {
+class NonEmptyListSpec extends FunSuite {
+  
+  val nel0 = NonEmptyList(1, 2, 3)
+  val nel1 = NonEmptyList("hoge", "fuga")
+  val nel3 = NonEmptyList(1, "piyo", 3.0, false)
+  val lst0 = List(3.14)
 
-  trait SetUp { self =>
-    val nel0 = NonEmptyList(1, 2, 3)
-    val nel1 = NonEmptyList("hoge", "fuga")
-    val nel3 = NonEmptyList(1, "piyo", 3.0, false)
-    val lst0 = List(3.14)
-  }
-
-  "::" should "concatenate one and one nonempty list" in new SetUp {
+  test("::") {
     assert(1 :: nel0 === NonEmptyList(1, 1, 2, 3))
   }
 
-  ":::" should "concatenate one list(including nonempty list) and another one" in new SetUp {
+  test(":::") {
     assert(nel1 ::: nel0 === NonEmptyList("hoge", "fuga", 1, 2, 3))
     assert(lst0 ::: nel0 === NonEmptyList(3.14, 1, 2, 3))
   }
 
-  "map" should "behave correctly" in new SetUp {
+  test("map") {
     assert(nel0.map(i => i + 1) === NonEmptyList(2, 3, 4))
   }
 
-  "flatMap" should "behave correctly" in new SetUp {
+  test("flatMap") {
     assert(nel0.flatMap(e => NonEmptyList(e, e + 1)) === NonEmptyList(1, 2, 2, 3, 3, 4))
   }
 
-  "collect" should "behave correctly" in new SetUp {
+  test("collect") {
     case class Piyo(value: String)
     case class Poyo(value: Int)
     val nel = NonEmptyList(Piyo("piyo"), Poyo(1), Poyo(2))
     assert(nel.collect { case Poyo(v) => v } === List(1, 2))
   }
 
-  "filter" should "filter NonEmptyList that is specified the conditions" in new SetUp {
+  test("filter") {
     assert(nel0.filter(i => i % 2 == 0) === List(2))
   }
 
-  "filterNot" should "filter NonEmptyList that is not specified the conditions" in new SetUp {
+  test("filterNot") {
     assert(nel0.filterNot(i => i % 2 == 0) === List(1, 3))
   }
 
-  "forall" should "behave correctly" in new SetUp {
+  test("forall") {
     assert(nel0.forall(n => n >= 0) === true)
     assert(nel3.forall(n => n == 1) === false)
   }
 
-  "unapply" should "behave correlctly" in new SetUp {
+  test("unapply") {
     val res = nel0 match {
       case NonEmptyList(x, xs) => x
     }
     assert(res === 1)
   }
 
-  "implicit conversion" should "behave correctly" in new SetUp {
+  test("implicit conversion") {
     import org.micchon.nonemptylist.implicits._
     import org.micchon.exception._
 
